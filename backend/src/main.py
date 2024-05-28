dea737e14073a272601fdcd9180e014cae885a95
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-05-28 16:52:54 +0100

    chore: budgets route on main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
   return "Hello world!"
 
+@app.route("/budgets", methods=["GET"])
+def get_budgets():
+  return 'budgets'
+
 if __name__ == "__main__":
   app.run(debug=True)
