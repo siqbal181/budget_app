d84e7bebb58109a4d31589deaf9a024f7d0b4888
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-05-30 16:50:12 +0100

    chore: edits to budget_routes

--- backend/src/budgets/budget_routes.py
+++ backend/src/budgets/budget_routes.py
@@ -13,19 +13,20 @@ def budget_items():
     amount = data.get('amount')
 
     if not category or not amount:
-      return jsonify({"error": "Category and amount are required"}), 400
+      return jsonify({"error": "Category and amount are required."}), 400
 
     try:
       db.execute("INSERT INTO budget (category, amount) VALUES (?, ?)", (category, amount))
       db.commit()
-      return jsonify({"message": "Budget item saved successfully"}), 201
+      return jsonify({"message": "Budget item saved successfully."}), 201
     except db.IntegrityError:
-       return jsonify({"error": str(e)}), 500
+       return jsonify({"error": "Unable to add budget item."}), 500
     
   elif request.method == "GET":
       db = get_db()
       try:
-          data = db.execute("SELECT * FROM budget").fetchall()
+          data = db.execute("SELECT * FROM budget").fetchall() # implicit cursor creation
+          # need to convert the data into dictionary list before passing into jsonify 
           budget_items = [{"id": row["id"], "category": row["category"], "amount": row["amount"]} for row in data]
 
           return jsonify(budget_items), 200
