07830aeceaee9480e6a03f8eea24d8124af7853d
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-05-30 15:42:06 +0100

    feat: budget routes setup with db

--- backend/src/budgets/budget_routes.py
+++ backend/src/budgets/budget_routes.py
@@ -1,7 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, request, flash, jsonify
+from src.db import get_db
 
 budgets_bp = Blueprint("budget_routes", __name__)
 
-@budgets_bp.route("/budgets")
-def get_budgets():
-  return 'budgets'
+@budgets_bp.route("/budgets", methods=["POST"])
+def post_budget_item():
+  db = get_db()
+
+  category, amount = request.json
+
+  try:
+    db.execute("INSERT INTO budget (category, amount) VALUES (?, ?)",
+    (category, amount))
+    db.commit()
+    return jsonify({"budgetItem saved successfully": category})
+  except db.IntegrityError:
+    error = f'Error in adding budget item'
+
+  flash(error)
