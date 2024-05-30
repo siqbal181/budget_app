1bb52520f80949de2543f6e10d86f0d30e8af5f4
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-05-30 10:10:24 +0100

    chore: further setup for __init__ file

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,10 +1,24 @@
 from flask import Flask
 from .budgets.budget_routes import budgets_bp
+import os
 
 
-def create_app():
+def create_app(test_config=None):
   app = Flask(__name__, instance_relative_config=True)
-  app.config.from_mapping
+  app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'budgetApp.sqlite'),
+  )
+
+  if test_config is None:
+    app.config.from_pyfile('config.py', silent=True)
+  else:
+    app.config.from_mapping(test_config)
+
+  try:
+    os.makedirs(app.instance_path)
+  except OSError:
+    pass
 
   app.register_blueprint(budgets_bp)
 
