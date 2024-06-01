0adcc2dea7ec13694f2501a8d77394e33f2218a8
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-06-01 13:56:11 +0100

    chore: test post route for spends

--- backend/tests/test_spend_routes.py
+++ backend/tests/test_spend_routes.py
@@ -1,5 +1,13 @@
 
 def test_get_spend_routes(client):
   response = client.get('/spends')
-  assert response.status == 200
+  assert response.status_code == 200
 
+def test_post_spend_route(client):
+  spend_item = {
+    "category": "Holiday",
+    "amount": 640
+  }
+
+  response = client.post('/spends', json=spend_item)
+  assert response.status_code == 201
