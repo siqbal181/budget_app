faaae16afc7d4a90b0ab327aa448ae74f8eeda17
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-06-03 15:04:29 +0100

    chore: test for deleting budget item failing

--- backend/tests/test_budget_routes.py
+++ backend/tests/test_budget_routes.py
@@ -25,3 +25,18 @@ def test_post_budgets_route(client):
     data = response_2.get_json()
     assert len(data) == 3
     assert data[-1]['category'] == 'Shopping'
+
+# def test_delete_budget_item_route(client):
+#     respone = client.get('/budgets')
+#     data_pre_delete = respone.get_json()
+#     assert len(data_pre_delete) == 2
+
+#     # check the delete response code
+#     delete_response = client.delete('/budgets', json={'id': 1})
+#     assert delete_response.status_code == 200
+
+#     # refetch data and check length is now 1
+#     delete_response = client.get('/budgets')
+#     data_post_delete = delete_response.get_json()
+#     print(data_post_delete)
+#     assert len(data_post_delete) == 1
