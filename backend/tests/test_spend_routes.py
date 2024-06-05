0f85228d16e4555a77c50c8133e137bc1b19e362
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-06-05 12:42:02 +0100

    chore: delete spend item test

--- backend/tests/test_spend_routes.py
+++ backend/tests/test_spend_routes.py
@@ -12,3 +12,18 @@ def test_post_spend_route(client):
 
     response = client.post('/spends', json=spend_item)
     assert response.status_code == 201
+
+def test_delete_spend_item_route(client):
+    # get data and check 2 items
+    respone = client.get('/spends')
+    data_pre_delete = respone.get_json()
+    assert len(data_pre_delete) == 2
+
+    # delete one item
+    delete_response = client.delete('/spends', json={'id': 1})
+    assert delete_response.status_code == 200
+
+    # get data again and check length is 1
+    delete_response = client.get('/spends')
+    data_post_delete = delete_response.get_json()
+    assert len(data_post_delete) == 1
