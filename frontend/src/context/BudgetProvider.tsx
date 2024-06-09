8c19b9972e112ba68d09e2f4cef10ac6400bfb37
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-06-09 16:28:46 +0100

    feat: set up budget context

---
+++ frontend/src/context/BudgetProvider.tsx
@@ -0,0 +1,39 @@
+import { createContext, useState } from "react";
+import { DataItem } from "../components/types";
+import { fetchBudgets } from "../services/budgetApiService";
+
+interface BudgetContextType {
+  budgetItems: DataItem[];
+  setBudgetItems: React.Dispatch<React.SetStateAction<DataItem[]>>;
+  getBudgets: () => Promise<void>;
+}
+
+export const BudgetContext = createContext<BudgetContextType>({
+  budgetItems: [],
+  setBudgetItems: () => {},
+  getBudgets: async () => {}
+})
+
+export const BudgetProvider = ({ children }: { children: React.ReactNode }) => {
+  const [budgetItems, setBudgetItems] = useState<DataItem[]>([]);
+
+  const getBudgets = async () => {
+    try {
+      const response = await fetchBudgets();
+
+      if (response.status !== 200) {
+        throw new Error ("Error in network response")
+      }
+      const budgets = response.data
+      setBudgetItems(budgets);
+    } catch (error) {
+      console.error('Error fetching budgets:', error)
+    }
+  }
+
+  return (
+    <BudgetContext.Provider value={{ budgetItems, setBudgetItems, getBudgets }}>
+      { children }
+    </BudgetContext.Provider>
+  )
+}
