b5c064d7094f4a49c5c0a1f65a187725639d8566
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-06-09 16:34:22 +0100

    chore: wrapped budget provider in app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css';
 import { BudgetOverviewPage } from './pages/BudgetOverviewPage/BudgetOverviewPage';
+import { BudgetProvider } from './context/BudgetProvider';
 
 function App() {
   return (
     <>
-      <BudgetOverviewPage title='Please enter your budgets here'/>
+      <BudgetProvider>
+        <BudgetOverviewPage title='Please enter your budgets here'/>
+      </BudgetProvider>
     </>
   );
 }
