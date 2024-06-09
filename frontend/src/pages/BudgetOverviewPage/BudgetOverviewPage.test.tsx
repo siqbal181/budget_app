9f57f15702e0c0c55c01678c4b816543843a6b0f
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-06-09 19:21:30 +0100

    chore: refactored budget tests

--- frontend/src/pages/BudgetOverviewPage/BudgetOverviewPage.test.tsx
+++ frontend/src/pages/BudgetOverviewPage/BudgetOverviewPage.test.tsx
@@ -20,7 +20,7 @@ const mockBudgetItems = [
   },
 ];
 
-function renderBudgetPage(component: React.ReactNode) {
+function renderWithContext(component: React.ReactNode) {
   return render(
     <BudgetContext.Provider
       value={{
@@ -34,38 +34,36 @@ function renderBudgetPage(component: React.ReactNode) {
   );
 }
 
+function renderBudgetPage() {
+  return renderWithContext(
+    <BudgetOverviewPage title="Please enter your budgets here" />
+  );
+}
+
 describe('When rendering the budget overview page', () => {
   test("The 'Please enter your budgets here' title should be displayed", async () => {
-    const { findByText } = render(
-      <BudgetOverviewPage title="Please enter your budgets here" />
-    );
+    const { findByText } = renderBudgetPage();
 
     const budgetTitle = await findByText('Please enter your budgets here');
     expect(budgetTitle).toBeInTheDocument();
   });
 
   test("The 'Add more categories' button should be displayed", async () => {
-    const { findByText } = render(
-      <BudgetOverviewPage title="Please enter your budgets here" />
-    );
+    const { findByText } = renderBudgetPage();
 
     const addMoreCategories = await findByText('Add more categories');
     expect(addMoreCategories).toBeInTheDocument();
   });
 
   test("The 'Save' button should be displayed", async () => {
-    const { findByText } = render(
-      <BudgetOverviewPage title="Please enter your budgets here" />
-    );
+    const { findByText } = renderBudgetPage()
 
     const saveButton = await findByText('Save');
     expect(saveButton).toBeInTheDocument();
   });
 
   test('getBudgets are called and budgets are returned', async () => {
-    const { findByText } = renderBudgetPage(
-      <BudgetOverviewPage title="Please enter your budgets here" />
-    );
+    const { findByText } = renderBudgetPage();
 
     const rentItem = await findByText('Rent');
     const billsItem = await findByText('Bills');
