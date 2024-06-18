c100db281d8ba98a679089b4cbf96266bff27434
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-06-18 20:09:14 +0100

    fix: moved the content into App Content and split off from App itself

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,47 +1,53 @@
 import './App.css';
 import { BudgetOverviewPage } from './pages/BudgetOverviewPage/BudgetOverviewPage';
-// import SpendOverviewPage from './pages/SpendOverViewPage/SpendOverviewPage';
+import SpendOverviewPage from './pages/SpendOverViewPage/SpendOverviewPage';
 import { BudgetProvider } from './context/BudgetProvider';
-// import { SpendProvider } from './context/SpendProvider';
-// import { useSpendContext } from './hooks/useSpendContext';
+import { SpendProvider } from './context/SpendProvider';
 import { useBudgetContext } from './hooks/useBudgetContext';
 import { useEffect } from 'react';
 import { NewCategory } from './components/types';
+import { useSpendContext } from './hooks/useSpendContext';
 
 interface AppProps {
   newCat: NewCategory;
 }
 
-function App({ newCat}: AppProps) {
+const App = ({ newCat }: AppProps) => {
+  return (
+    <BudgetProvider>
+      <SpendProvider>
+        <AppContent newCat={newCat} />
+      </SpendProvider>
+    </BudgetProvider>
+  );
+};
+
+const AppContent = ({ newCat }: AppProps) => {
   const { getBudgets, budgetItems } = useBudgetContext();
-  // const { getSpends, spendItems } = useSpendContext();
+  const { getSpends, spendItems } = useSpendContext();
 
   useEffect(() => {
     getBudgets();
   }, [getBudgets]);
 
-  // useEffect(() => {
-  //   getSpends();
-  // }, [getSpends]);
+  useEffect(() => {
+    getSpends();
+  }, [getSpends]);
 
   return (
     <>
-      <BudgetProvider>
-        {/* <SpendProvider> */}
-          <BudgetOverviewPage
-            title="Please enter your budgets"
-            data={budgetItems}
-            newCat={newCat}
-          />
-          {/* <SpendOverviewPage
-            title="Please enter your spends"
-            data={spendItems}
-            newCat={newCat}
-          /> */}
-        {/* </SpendProvider> */}
-      </BudgetProvider>
+      <BudgetOverviewPage
+        title="Please enter your budgets"
+        data={budgetItems}
+        newCat={newCat}
+      />
+      <SpendOverviewPage
+        title="Please enter your spends"
+        data={spendItems}
+        newCat={newCat}
+      />
     </>
   );
-}
+};
 
 export default App;
